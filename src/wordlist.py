ebb097b23b0ec2684ad686ba9ecb4297f5ec2f3d
archie <57256763+Bl4omArchie@users.noreply.github.com>
2022-12-17 19:23:44 +0100

    Update wordlist.py

--- src/wordlist.py
+++ src/wordlist.py
@@ -2,20 +2,30 @@ from random import randint
 import requests
 
 
-""" A special object for manage the wordlist """
-
 class Wordlist():
+    """ A special object for manage the wordlist 
+    a Wordlist object is:
+        - a wordlist grabbed from a link
+        - the size of the wordlist
+        - a dictionnary with every words and their stats
+        - one random wordle (the word to be guess)
+    """
+
     def __init__(self, link) -> None:
         r = requests.get(link)
-        self.wlist = r.text.split("\n")
-        self.wlist_size = len(self.wlist)
+        r = r.text.split("\n")
+        
+        self.word_count = len(r)
+        self.to_guess = r[randint(0, self.word_count)]
+
+        self.wdict = {}
+        for word in r:
+            self.wdict[word] = 1
 
     
     def split_word(self, word): #split every letter of the given word. ex: shark -> ['s', 'h', 'a', 'r', 'k']
         return [*word]
 
-    def word_in_list(self, word):
-        return word in self.wlist
 
-    def get_wordle(self):
-        return self.wlist[randint(0, self.wlist_size)]
+    def word_in_list(self, word):
+        return word in self.wdict
